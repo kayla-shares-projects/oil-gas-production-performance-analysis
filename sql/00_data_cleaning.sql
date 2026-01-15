3c6c87bc098c2fdc7ed6be7a3e0eef3e5a4b6d18
kayla-shares-projects <kalypsokaizen@gmail.com>
2026-01-15 16:55:15 +0100

    Update 00_data_cleaning.sql

--- sql/00_data_cleaning.sql
+++ sql/00_data_cleaning.sql
@@ -2,3 +2,16 @@
 -- Purpose: Prepare production data for analytical querying
 -- Source: Raw production reporting dataset
 -- Output: Cleaned production table for analysis
+
+ALTER TABLE production ALTER COLUMN "Production Date"
+TYPE date
+USING "Production Date"::date
+
+UPDATE production
+SET "Volume" = REPLACE("Volume",',','')
+
+ALTER TABLE production ALTER COLUMN "Volume"
+TYPE bigint
+USING "Volume"::bigint
+
+'NEXT CHANGE TABLE NAMES'
