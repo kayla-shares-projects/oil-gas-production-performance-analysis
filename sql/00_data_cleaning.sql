7888feee1645f230d5d02b3fff7d8557855773a8
kayla-shares-projects <kalypsokaizen@gmail.com>
2026-01-19 15:16:13 +0100

    Update 00_data_cleaning.sql

--- sql/00_data_cleaning.sql
+++ sql/00_data_cleaning.sql
@@ -3,10 +3,12 @@
 -- Source: Raw production reporting dataset
 -- Output: Cleaned production table for analysis
 
+--Change table type from text to date
 ALTER TABLE production ALTER COLUMN "Production Date"
 TYPE date
 USING "Production Date"::date
 
+--Remove commas from numbers and changing data type from text to bigint
 UPDATE production
 SET "Volume" = REPLACE("Volume",',','')
 
@@ -14,4 +16,13 @@ ALTER TABLE production ALTER COLUMN "Volume"
 TYPE bigint
 USING "Volume"::bigint
 
-'NEXT CHANGE TABLE NAMES'
+--Change column names to remove quotations
+ALTER TABLE production
+RENAME COLUMN "Commodity" to Commodity
+ALTER TABLE production
+RENAME COLUMN "Volume" to Volume
+ALTER TABLE production
+RENAME COLUMN "Disposition Description" to Disposition_Description
+ALTER TABLE production
+RENAME COLUMN "Disposition Code" to Disposition_Code
+--etc...
