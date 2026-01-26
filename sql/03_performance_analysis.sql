bdf826ded06d0c7a321e6d8a831701a410148319
kayla-shares-projects <kalypsokaizen@gmail.com>
2026-01-26 14:30:57 +0100

    Create 03_performance_analysis.sql

---
+++ sql/03_performance_analysis.sql
@@ -0,0 +1,3 @@
+--Question 1: trend analysis
+
+--Comments: Our time window was defined by the last 5 years on our records. and growth was defined by avereage year over year change, and looking at a trend slope.
