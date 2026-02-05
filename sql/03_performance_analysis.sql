5405bde91eea9e618cdb8170228ebbd1942366fd
kayla-shares-projects <kalypsokaizen@gmail.com>
2026-02-05 11:23:21 +0100

    Answer question 1

--- sql/03_performance_analysis.sql
+++ sql/03_performance_analysis.sql
@@ -1,3 +1,29 @@
---Question 1: trend analysis
+--Question 1: Which counties are experiencing sustsained growth or sustained decline in production
+WITH 
+	yearlyprod AS(
+		SELECT sum(volume) as sumtime, fips_code, extract(year from production_date) as by_year 
+		FROM performance_analysis
+		GROUP BY by_year, fips_code
+	),
+	yearlydiff AS (
+		SELECT 
+			sumtime, 
+			fips_code, 
+			by_year, 
+			sumtime -
+				LAG(sumtime) OVER(
+				PARTITION BY fips_code
+				ORDER BY by_year) as yearlychange
+		FROM yearlyprod	
+	)
+SELECT avg(yearlychange) as avg_yearly_change, fips_code 
+FROM yearlydiff
+GROUP BY fips_code
 
---Comments: Our time window was defined by the last 5 years on our records. and growth was defined by avereage year over year change, and looking at a trend slope.
+--When paired with the below query we can compare the average yearly difference in production to the amount of years the asset has been on our records. This is to avoid attributing spikes because of recent open or closure to overall county trends
+SELECT COUNT(DISTINCT EXTRACT(year from production_date)), 
+fips_code 
+FROM production
+GROUP BY fips_code
+
+--Comments: Our time window was defined by the 10 years on our records. And growth was defined by average year over year change, and looking at a trend slope.
