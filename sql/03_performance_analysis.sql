f5fbc4cfd99f5a89db252e7580d872ae794346c9
kayla-shares-projects <kalypsokaizen@gmail.com>
2026-02-20 10:33:53 +0100

    Update 03_performance_analysis.sql

--- sql/03_performance_analysis.sql
+++ sql/03_performance_analysis.sql
@@ -27,3 +27,46 @@ FROM production
 GROUP BY fips_code
 
 --Comments: Our time window was defined by the 10 years on our records. And growth was defined by average year over year change, and looking at a trend slope.
+
+	
+--Question 2: Which counties have already passed peak production, and which are still growing?
+WITH 
+	yearlyprod AS(
+		SELECT sum(volume) as yearlytotal, fips_code, extract(year from production_date) as byyear 
+		FROM performance_analysis
+		GROUP BY byyear, fips_code
+	),
+	rankedprod AS(
+		SELECT 
+			fips_code, 
+			yearlytotal, 
+			byyear,
+			RANK()OVER(
+				PARTITION BY fips_code
+				ORDER BY yearlytotal DESC
+			) AS prodrank
+		FROM yearlyprod	
+	),
+	latestyear AS (
+		SELECT
+			fips_code,
+			MAX(byyear)AS maxyear
+		FROM yearlyprod
+		GROUP BY fips_code
+	)
+
+
+SELECT 
+	rp.fips_code,
+	rp.byyear AS peakyear,
+	rp.yearlytotal AS peakvalue,
+	ly.maxyear,
+	CASE 
+    	WHEN rp.byyear = ly.maxyear THEN 'At Peak'
+    	ELSE 'Post-Peak'
+	END as Status
+FROM rankedprod rp
+JOIN latestyear ly ON rp.fips_code = ly.fips_code
+WHERE rp.prodrank = 1
+
+--Comments:
